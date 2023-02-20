b0b5aed7c13e5d9727d9b403a400d9c40b8627c2
Leander Pfeiffer <leander.pfeiffer@tum.de>
2023-02-20 13:08:16 +0100

    Added implementation of Task 6,8 and 9

---
+++ Homework3/CFiles/OwnCode/Controller.c
@@ -0,0 +1,25 @@
+switch(task_number){
+    case 6:
+        theta_goal = atan2(yg - y0,xg - x0) * (180/PI);
+        w = K_Phi * (theta_goal - theta);
+        v = 0;
+        break;    
+
+    case 8:
+        w = 0;
+        
+        d_0 = cos(theta * PI/180) * (x0 - x) + sin(theta * PI/180) * (y0 - y);
+        v =  K_omega * d_0;
+        break;
+
+    case 9:
+        theta_goal = atan2(yg - y0,xg - x0) * (180/PI);
+        w = K_Phi * (theta_goal - theta);
+        
+        d_0 = cos(theta * PI/180) * (x0 - x) + sin(theta * PI/180) * (y0 - y);
+        v =  K_omega * d_0;
+        break;
+}
+
+left = v - w/2;
+right = v + w/2;
---
+++ Homework3/CFiles/OwnCode/OwnVariables.c
@@ -0,0 +1,10 @@
+double v = 0;
+double w = 0;
+
+double theta_goal = 0;
+double d_0 = 0;
+
+const double K_Phi = 0.5;
+const double K_omega = 10;
+
+const int task_number = 6;
